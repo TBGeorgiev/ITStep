263ed2f5e0bc64a585bdcead675b7a612f3ac48b
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-04-10 10:28:22 +0300

    Create 01.SimpleArrayEdit.java

---
+++ Exercises/Arrays/01.SimpleArrayEdit.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(reader.readLine());
+		int[] array = new int[n];
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = Integer.parseInt(reader.readLine());
+			if (array[i] % 2 == 0) {
+				array[i] = 0;
+			}
+		}
+		
+		System.out.println(Arrays.toString(array).replace("[", "").replace("]", "").replaceAll(",", ""));
+	}
+
+}
