81d6ddcc8476b427cf6a30c68388b955ab7b2f31
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-04-11 12:57:23 +0300

    Create 03.PythagoreanNumbers.java

---
+++ Exercises/Arrays/11.04/03.PythagoreanNumbers.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PythagoreanNumbers {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(reader.readLine());
+		
+		List<Integer> listOfNumbersToCheck = new ArrayList<>();
+		
+		for (int i = 0; i < n; i++) {
+			listOfNumbersToCheck.add(Integer.parseInt(reader.readLine()));
+		}
+		
+		boolean isFound = false;
+		
+		for (int i = 0; i < listOfNumbersToCheck.size(); i++) {
+			for (int j = 0; j < listOfNumbersToCheck.size(); j++) {
+				for (int j2 = 0; j2 < listOfNumbersToCheck.size(); j2++) {
+					int num1 = listOfNumbersToCheck.get(i);
+					int num2 = listOfNumbersToCheck.get(j);
+					int num3 = listOfNumbersToCheck.get(j2);
+					
+					if (num2 * num2 <= num3 * num3  && (num2 * num2) + (num3 * num3) == num1 * num1) {
+						isFound = true;
+						System.out.printf("%d * %d + %d * %d = %d * %d%n", num2, num2, num3, num3, num1 , num1);
+					}
+					
+				}
+			}
+		}
+		
+		if (!isFound) {
+			System.out.println("No");
+		}
+	}
+
+}
