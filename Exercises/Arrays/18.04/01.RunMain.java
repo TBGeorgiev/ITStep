c94c05e9d7603365642defb3f7f5b0032bd34678
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-04-18 16:24:38 +0300

    Update 01.RunMain.java

--- Exercises/Arrays/18.04/01.RunMain.java
+++ Exercises/Arrays/18.04/01.RunMain.java
@@ -1,7 +1,9 @@
 package com.seeburger.run;
 
+import java.io.IOException;
 import java.util.Scanner;
 
+import com.seeburger.filemanipulation.ReadFromPropertyFile;
 import com.seeburger.sort.*;
 
 
@@ -11,9 +13,9 @@ public class RunMain
 	
 	private static String[] args;
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
-		listFilesFromDirectory();
+		ReadFromPropertyFile.readPropertyFile();
 	}
 	
 	
