bc45d76918dda83868ee54691aca1637aa382710
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-04-24 15:34:55 +0300

    Update 01.Main.java

--- Exercises/OOP/19.04/01.Main.java
+++ Exercises/OOP/19.04/01.Main.java
@@ -3,7 +3,17 @@ package com.seeburger.run;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+
+import javax.swing.JOptionPane;
 
 import com.seeburger.filemanipulation.ConnectionToDatabase;
 import com.seeburger.filemanipulation.CopyFile;
@@ -11,6 +21,7 @@ import com.seeburger.filemanipulation.CreateTables;
 import com.seeburger.filemanipulation.ReadFromPropertyFile;
 import com.seeburger.sort.*;
 import com.seeburger.students.ListOfStudents;
+import com.seeburger.students.Student;
 
 
 public class RunMain
@@ -21,23 +32,106 @@ public class RunMain
 
 	public static void main(String[] args) throws IOException
 	{
-		Connection connection = null;
-		try {
-//			System.out.println(System.getProperty("user.dir"));
-			ConnectionToDatabase database = new ConnectionToDatabase();
-			connection = database.databaseConnection();
-			CreateTables tables = new CreateTables();
-			tables.createTable(connection);
-			tables.listDatabase(connection);
-			System.out.println(System.getProperty("java.version"));
-			
-			
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("Error");
+		
+//		readStudentsFromFileAndPrint("C:\\Users\\MKA\\Documents\\eclipse\\StudentData.txt");
+		readStudentsFromFileAndPutInHashMapToPrint("C:\\\\Users\\\\MKA\\\\Documents\\\\eclipse\\\\StudentData.txt");
+		
+		
+		
+		
+//		Runnable runnable = new Runnable()
+//		{
+//			
+//			@Override
+//			public void run()
+//			{
+//				long start = System.currentTimeMillis();
+//				System.out.println("==========================================================================================T1 - Start time: "
+//				+ start);
+//				File toCopy = CopyFile.findFileToCopy("C:\\Cisco Packet Tracer 7.0");
+//				long end = System.currentTimeMillis();;
+//				System.out.println("==========================================================================================T1 - End time: "
+//				+ end);
+//				System.out.println("==========================================================================================T1 - Total time: "
+//				+ (end - start));
+//			}
+//		};
+//		
+//		
+//		
+//		Runnable runnable2 = new Runnable()
+//		{
+//			
+//			@Override
+//			public void run()
+//			{
+//				long start = System.currentTimeMillis();
+//				System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++T2 - Start time: "
+//				+ start);
+//				File toCopy = CopyFile.findFileToCopy("C:\\Cisco Packet Tracer 7.0");
+//				long end = System.currentTimeMillis();;
+//				System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++T2 - End time: "
+//				+ end);
+//				System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++T2 - Total time: " + (end - start));
+//				
+//			}
+//		};
+//		
+//		runnable.run();
+//		runnable2.run();
+		
+//		addAndListStudentsAndSaveToFile("C:\\Users\\MKA\\Documents\\eclipse\\students.txt");
+//		readStudentsFromFileAndPrint("C:\\Users\\MKA\\Documents\\eclipse\\students.txt");
+//		Connection connection = null;
+//		try {
+////			System.out.println(System.getProperty("user.dir"));
+//			ConnectionToDatabase database = new ConnectionToDatabase();
+//			connection = database.databaseConnection("Properties");
+//			CreateTables tables = new CreateTables();
+//			
+//			
+//			String sqlStatement = "CREATE TABLE IF NOT EXISTS Properties "
+//					+ "(Key TEXT NOT NULL,"
+//					+ " Value TEXT NOT NULL)";
+//			
+//			String sqlStatementFill = "INSERT INTO Properties VALUES ('ip:address')";
+//			
+//			printTable(connection, "Properties");
+//			
+//			
+////			tables.executeSqlStatement(connection, sqlStatementFill);
+//			
+////			connection.commit();
+//			connection.close();
+//			
+////			tables.createTable(connection);
+////			tables.listDatabase(connection);
+////			System.out.println(System.getProperty("java.version"));
+//			
+//			
+//			
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//			System.out.println("Error");
+//		}
+		
+	}
+	
+	private static void printTable(Connection connection, String tableName) throws SQLException
+	{
+		Statement statement = connection.createStatement();
+		String sql = "SELECT * FROM " + tableName;
+		ResultSet resultSet = statement.executeQuery(sql);
+		
+		while (resultSet.next())
+		{
+			System.out.println(resultSet.getString(1));
+			System.out.println(resultSet.getString(2));
 		}
 		
+		resultSet.close();
+		statement.close();
 	}
 	
 	
@@ -49,13 +143,65 @@ public class RunMain
 	}
 	
 	
-	private static void addAndListStudents()
+	private static void readStudentsFromFileAndPutInHashMapToPrint(String location) throws IOException
+	{
+		ListOfStudents listOfStudents = new ListOfStudents();
+		listOfStudents.addStudentsFromFile(location);
+		TreeMap<Integer, ArrayList<Student>> toPrint = listOfStudents.getMapWithStudents();
+		
+		for (Map.Entry entry : toPrint.entrySet()) {
+			System.out.println("Group " + entry.getKey());
+			ArrayList<Student> students = (ArrayList<Student>) entry.getValue();
+			for (Student student : students) {
+				System.out.println(student.toString());
+			}
+		}
+		
+//		Iterator iterator = toPrint.entrySet().iterator();
+//		while (iterator.hasNext())
+//		{
+//			Map.Entry pair = (Map.Entry)iterator.next();
+//	        System.out.println("Group: " + pair.getKey());
+//	        ArrayList<Student> students = (ArrayList<Student>) pair.getValue();
+//	        Iterator<Student> iterator2 = students.iterator();
+//	        while (iterator2.hasNext())
+//			{
+//				System.out.println(iterator2.next());
+//			}
+//	        iterator.remove();
+//		}
+		
+		int answer = JOptionPane.showConfirmDialog(null, "Do you want to save the students?", null, JOptionPane.YES_NO_OPTION);
+		if (answer == JOptionPane.YES_OPTION) {
+			listOfStudents.saveStudentsToFile("C:\\Users\\MKA\\Documents\\eclipse\\test\\StudentsTest.txt");
+			System.out.println("Save complete.");
+		}
+		
+		
+	}
+	
+	private static void readStudentsFromFileAndPrint(String location) throws IOException
+	{
+		
+		ListOfStudents listOfStudents = new ListOfStudents();
+		listOfStudents.addStudentsFromFile(location);
+//		ArrayList<Student> sorted = listOfStudents.sortStudents();
+		ArrayList<Student> sortedByAverages = listOfStudents.sortByAverageGrade();
+//		ArrayList<Student> getStudents = listOfStudents.getStudentsList();
+		listOfStudents.printAllStudentsToConsole(sortedByAverages);
+	}
+	
+	private static void addAndListStudentsAndSaveToFile(String fileLoc) throws IOException
 	{
 		ListOfStudents listOfStudents = new ListOfStudents();
 		
 		listOfStudents.addMultipleStudents();
 		
-		listOfStudents.printAllStudentsToConsole();
+		ArrayList<Student> sorted = listOfStudents.sortStudents();
+		
+		listOfStudents.saveStudentsToFile(fileLoc);
+		
+		listOfStudents.printAllStudentsToConsole(sorted);
 	}
 	
 	
