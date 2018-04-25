c0ad04059dae4dce54023015de8c654182ea8d61
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-04-25 16:48:23 +0300

    Create 03.Teacher.java

---
+++ Exercises/OOP/25.04/03.Teacher.java
@@ -0,0 +1,14 @@
+package com.seeburger.oop;
+
+import com.seeburger.exceptions.InvalidAgeException;
+
+public class Teacher extends Person
+{
+
+	public Teacher(String name, int age, char gender) throws InvalidAgeException
+	{
+		super(name, age, gender);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
