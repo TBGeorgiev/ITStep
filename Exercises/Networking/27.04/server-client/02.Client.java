df0dd463a705e6daf16cc0a70bd339c2f6fbd458
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-03 15:05:01 +0300

    Update 02.Client.java

--- Exercises/Networking/27.04/server-client/02.Client.java
+++ Exercises/Networking/27.04/server-client/02.Client.java
@@ -1,4 +1,7 @@
+package com.seeburger.networking;
+
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,27 +17,54 @@ public class Client {
 	}
 	
 	public void connectToServer(String ipAddress, int port) throws IOException {
-		try {
-			String toSend = "";
-			while (!(toSend = reader.readLine()).equals("stop")) {
-				
-				Socket socket = new Socket(ipAddress, port);
-				DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-				
-				dataOutputStream.writeUTF(toSend);
-				dataOutputStream.flush();
-				dataOutputStream.close();
-				socket.close();
-				
-				
-			}
+		Socket s=new Socket(ipAddress , port);  
+		DataInputStream din=new DataInputStream(s.getInputStream());  
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+		  
+		String str="",str2="";  
+		while(!str.equals("stop")){  
+			str=br.readLine();  
+			dout.writeUTF(str);  
+			dout.flush();  
+			str2=din.readUTF();  
+			System.out.println("Server says: "+str2);  
+		}  
+		  
+		dout.close();  
+		s.close();  
+		
+		
+		
+		
+		
+		
+//		try {
+//			String toSend = "";
+//			String toSend2 = "";
+//			while (!(toSend = reader.readLine()).equals("stop")) {
+//				
+//				Socket socket = new Socket(ipAddress, port);
+//				DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+//				DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+//				
+//				toSend2 = dataInputStream.readUTF();
+//				System.out.println(toSend2);
+//				
+//				dataOutputStream.writeUTF(toSend);
+//				dataOutputStream.flush();
+//				dataOutputStream.close();
+//				socket.close();
+//				
+//				
+//			}
 			
 			
 			
-			
-		} catch (UnknownHostException e) {
-			// TODO: handle exception
-		}
+//			
+//		} catch (UnknownHostException e) {
+//			// TODO: handle exception
+//		}
 	}
 
 }
