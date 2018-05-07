dd45194013883086655b8de9b0c90ea203bb187c
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-07 17:01:01 +0300

    Update 02.Client.java

--- Exercises/Networking/27.04/server-client/02.Client.java
+++ Exercises/Networking/27.04/server-client/02.Client.java
@@ -1,5 +1,3 @@
-package com.seeburger.networking;
-
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -7,10 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Client {
 	
 	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private ExecutorService executorService = Executors.newFixedThreadPool(100);
 	
 	public Client() {
 		
@@ -20,15 +21,20 @@ public class Client {
 		Socket s=new Socket(ipAddress , port);  
 		DataInputStream din=new DataInputStream(s.getInputStream());  
 		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		RunnableForClient runnableForClient = new RunnableForClient(din, s);
+		executorService.execute(runnableForClient);
 		  
 		String str="",str2="";  
 		while(!str.equals("stop")){  
 			str=br.readLine();  
 			dout.writeUTF(str);  
-			dout.flush();  
-			str2=din.readUTF();  
-			System.out.println("Server says: "+str2);  
+			dout.flush();
+
+
+			
+//			str2=din.readUTF();  
+//			System.out.println("Server says: "+str2);  
 		}  
 		  
 		dout.close();  
