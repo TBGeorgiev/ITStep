de8ead16234a7017605d2f7025838f12810fc7bc
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-09 16:10:54 +0300

    Create 06.User.java

---
+++ Exercises/Networking/27.04/server-client/experimental/06.User.java
@@ -0,0 +1,57 @@
+package com.seeburger.networking;
+
+import java.io.Serializable;
+
+public class User implements Serializable
+{
+	private String ipAddress;
+	private String name;
+	private String password;
+	private String email;
+	
+	
+	public User(String ipAddress, String name, String password, String email)
+	{
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.ipAddress = ipAddress;
+	}
+
+
+	public String getName()
+	{
+		return name;
+	}
+
+
+
+
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+
+	
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+
+	@Override
+	public String toString()
+	{
+		return this.ipAddress + ": Name: " + this.name + " Password: " + this.password + " Email: " + this.email;
+	}
+
+
+	
+	
+	
+	
+
+}
