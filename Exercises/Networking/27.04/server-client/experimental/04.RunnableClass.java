c1abc68d451e3b2692ae885bcda7044cde7ca882
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-09 16:10:06 +0300

    Create 04.RunnableClass.java

---
+++ Exercises/Networking/27.04/server-client/experimental/04.RunnableClass.java
@@ -0,0 +1,117 @@
+package com.seeburger.networking;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RunnableClass implements Runnable {
+	private int port;
+	private Socket socket;
+	private ServerSocket serverSocket;
+	private ExecutorService executorService = Executors.newFixedThreadPool(100);
+	private String name;
+	
+	private ArrayList<Socket> sockets;
+	
+	
+	public RunnableClass(int port, Socket socket, ServerSocket serverSocket, ArrayList<Socket> sockets, String name) {
+		this.name = name;
+		this.port = port;
+		this.socket = socket;
+		this.serverSocket = serverSocket;
+		this.sockets = sockets;
+	}
+
+	@Override
+	public void run() {
+		startServer(this.socket, this.serverSocket);
+		
+	}
+	
+	
+	public void startServer(Socket socket, ServerSocket ss) {
+		try {
+			
+//			ServerSocket ss=new ServerSocket(port);  
+//			Socket s=ss.accept();  
+			DataInputStream din=new DataInputStream(socket.getInputStream());  
+			DataOutputStream dout=new DataOutputStream(socket.getOutputStream());  
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+			  
+			String str="";  
+			while(!str.equals("stop")){  
+				str=din.readUTF();  
+				System.out.println(socket.getInetAddress() + " : " + str);  
+//				str2 = str;
+//				str2=br.readLine();  
+//				dout.writeUTF(str2);  
+//				dout.flush();  
+				for (int i = 0; i < sockets.size(); i++) {
+					DataOutputStream dout2 = new DataOutputStream(sockets.get(i).getOutputStream());
+					dout2.writeUTF(socket.getInetAddress() + ": " + name + ": " + str);
+					dout2.flush();
+					
+				}
+			}  
+			
+			din.close();  
+			socket.close();  
+			ss.close();  
+			
+//			String mString = "";
+//			String string2 = "";
+//			
+//			DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+//			DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+//			mString = (String)dataInputStream.readUTF();
+//			
+//			string2 = mString;
+//			dataOutputStream.writeUTF(mString);
+//			dataOutputStream.flush();
+////			if (mString.equals("stop")) {
+////				break;
+////			}
+//			System.out.println(socket.getInetAddress().toString() + ": " + Thread.currentThread().getName() + "message= " +  mString);
+//			
+////			DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+////			dataOutputStream.writeUTF(mString);
+////			dataOutputStream.flush();
+////			dataOutputStream.close();
+//			
+//			
+////			ChatWindowWatcher chatWindowWatcher = new ChatWindowWatcher(socket.getInetAddress().toString() + ": " + mString);
+////			executorService.execute(chatWindowWatcher);
+//			
+//			
+//			dataInputStream.close();
+//			while (true) {
+//				
+//				
+//				
+//				
+////				socket = serverSocket.accept();
+//
+//				
+//				
+//			}
+//			System.out.println("broke out of while loop in runnable");
+			
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+	}
+	
+	
+
+}
