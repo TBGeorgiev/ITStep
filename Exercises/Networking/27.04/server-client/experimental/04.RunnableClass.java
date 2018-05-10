7913f46a6eeb8f05769c46d0d6fb1b8b30e12ca3
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-10 16:45:33 +0300

    Update 04.RunnableClass.java

--- Exercises/Networking/27.04/server-client/experimental/04.RunnableClass.java
+++ Exercises/Networking/27.04/server-client/experimental/04.RunnableClass.java
@@ -35,16 +35,44 @@ public class RunnableClass implements Runnable {
 	public void startServerThread(Socket socket, ServerSocket ss) {
 		try {
 			
+			
+			
 //			ServerSocket ss=new ServerSocket(port);  
 //			Socket s=ss.accept();  
 			DataInputStream din = new DataInputStream(socket.getInputStream());  
 			DataOutputStream dout = new DataOutputStream(socket.getOutputStream());  
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+			String joined = name + " has joined the chat.";
+			System.out.println(joined);
+			for (int i = 0; i < sockets.size(); i++) {
+				if (sockets.get(i).isClosed()) {
+					sockets.remove(i);
+					i--;
+				} else {
+					dout = new DataOutputStream(sockets.get(i).getOutputStream());
+//					dout.writeUTF(socket.getInetAddress() + ": " + name + ": " + str);
+					dout.writeUTF(joined);
+					dout.flush();
+					
+				}
+			}
 			  
-			String str = "";  
+			String str = ""; 
 			
-			while(!(str = din.readUTF()).equals("exit")){  
-				System.out.println(socket.getInetAddress() + " " + name + " : " + str);  
+			boolean quit = false;
+			
+//			while(!(str = din.readUTF()).equals("exit")){  
+			while (!quit) {
+				str = din.readUTF();
+				String toSend = "";
+				if (str.equals("exit")) {
+					toSend = name + " has left the chat room.";
+					quit = true;
+				} else {
+					toSend = socket.getInetAddress() + ": " + name + ": " + str;
+				}
+				System.out.println(toSend);
+//				System.out.println(socket.getInetAddress() + " " + name + " : " + str);  
 				
 				for (int i = 0; i < sockets.size(); i++) {
 					if (sockets.get(i).isClosed()) {
@@ -52,7 +80,8 @@ public class RunnableClass implements Runnable {
 						i--;
 					} else {
 						dout = new DataOutputStream(sockets.get(i).getOutputStream());
-						dout.writeUTF(socket.getInetAddress() + ": " + name + ": " + str);
+//						dout.writeUTF(socket.getInetAddress() + ": " + name + ": " + str);
+						dout.writeUTF(toSend);
 						dout.flush();
 						
 					}
@@ -60,7 +89,7 @@ public class RunnableClass implements Runnable {
 				}
 				
 			}
-			System.out.println(name + " has left the chat room.");
+//			System.out.println(name + " has left the chat room.");
 			
 			dout.close();
 			din.close();  
