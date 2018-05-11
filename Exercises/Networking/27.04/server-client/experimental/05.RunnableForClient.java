49db5aa8872cd08f2d4c9c3895e7171378d55edf
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-11 17:05:34 +0300

    Update 05.RunnableForClient.java

--- Exercises/Networking/27.04/server-client/experimental/05.RunnableForClient.java
+++ Exercises/Networking/27.04/server-client/experimental/05.RunnableForClient.java
@@ -4,55 +4,52 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class RunnableForClient implements Runnable {
+public class RunnableForClient implements Runnable
+{
 	private DataInputStream dataInputStream;
 	private Socket socket;
 
-	public RunnableForClient(DataInputStream dataInputStream, Socket socket) {
+	public RunnableForClient(DataInputStream dataInputStream, Socket socket)
+	{
 		this.dataInputStream = dataInputStream;
 		this.socket = socket;
-		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
-	public void run() {
+	public void run()
+	{
 		String string = null;
-		
-		while (true) {
-			try {
-				
+		while (true)
+		{
+			try
+			{
 				string = dataInputStream.readUTF();
-				if (string != null && string.equals("exit")) {
+				if (string != null && string.equals("exit"))
+				{
 					System.out.println("Closing chat.");
 					try
 					{
 						Thread.currentThread().sleep(500);
 					} catch (InterruptedException e)
 					{
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					System.exit(0);
-//					socket.close();
-//					dataInputStream.close();
+					// socket.close();
+					// dataInputStream.close();
 					break;
-				}
-				else if (string != null) {
+				} else if (string != null)
+				{
 					System.out.println(string);
 					string = null;
-					
+
 				}
-//				if (string.length() > 0) {
-//					string = "";
-//				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			} catch (IOException e)
+			{
 				System.out.println("IOFException inside RunnableClient");
 				e.printStackTrace();
 				break;
 			}
 		}
-		
 	}
-
 }
