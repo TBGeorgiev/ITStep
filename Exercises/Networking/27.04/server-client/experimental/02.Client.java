159c78f680fd4748de038cebd326687170917395
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-11 17:04:37 +0300

    Update 02.Client.java

--- Exercises/Networking/27.04/server-client/experimental/02.Client.java
+++ Exercises/Networking/27.04/server-client/experimental/02.Client.java
@@ -10,72 +10,35 @@ import java.net.UnknownHostException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Client {
-	
+public class Client
+{
 	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	private ExecutorService executorService = Executors.newFixedThreadPool(100);
-	
-	public Client() {
-		
+
+	public Client()
+	{
 	}
-	
-	public void connectToServer(String ipAddress, int port) throws IOException {
-	
-		Socket s = new Socket(ipAddress , port);  
-		DataInputStream din = new DataInputStream(s.getInputStream());  
-		DataOutputStream dout = new DataOutputStream(s.getOutputStream());  
+
+	public void connectToServer(String ipAddress, int port) throws IOException
+	{
+		Socket s = new Socket(ipAddress, port);
+		DataInputStream din = new DataInputStream(s.getInputStream());
+		DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		RunnableForClient runnableForClient = new RunnableForClient(din, s);
 		executorService.execute(runnableForClient);
-		  
-		String str="",str2="";  
-		while(!str.equals("exit")){  
-			str=br.readLine();  
-			dout.writeUTF(str);  
+
+		String str = "", str2 = "";
+		while (!str.equals("exit"))
+		{
+			str = br.readLine();
+			dout.writeUTF(str);
 			dout.flush();
-
-
-			
-//			str2=din.readUTF();  
-//			System.out.println("Server says: "+str2);  
 		}
 		System.out.println("Chat stopped from client.");
-		executorService.shutdown();  
-		dout.close();  
+		executorService.shutdown();
+		dout.close();
 		din.close();
-		s.close();  
-		
-		
-		
-		
-		
-		
-//		try {
-//			String toSend = "";
-//			String toSend2 = "";
-//			while (!(toSend = reader.readLine()).equals("stop")) {
-//				
-//				Socket socket = new Socket(ipAddress, port);
-//				DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-//				DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-//				
-//				toSend2 = dataInputStream.readUTF();
-//				System.out.println(toSend2);
-//				
-//				dataOutputStream.writeUTF(toSend);
-//				dataOutputStream.flush();
-//				dataOutputStream.close();
-//				socket.close();
-//				
-//				
-//			}
-			
-			
-			
-//			
-//		} catch (UnknownHostException e) {
-//			// TODO: handle exception
-//		}
+		s.close();
 	}
-
 }
