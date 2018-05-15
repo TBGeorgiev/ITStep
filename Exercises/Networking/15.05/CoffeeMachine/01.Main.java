530732b63fae82596690c561fc42f95060309a7e
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-15 15:56:08 +0300

    Create 01.Main.java

---
+++ Exercises/Networking/15.05/CoffeeMachine/01.Main.java
@@ -0,0 +1,17 @@
+package com.seeburger.run;
+
+import com.seeburger.client.Client;
+import com.seeburger.server.Server;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Server server = new Server();
+//		Client client = new Client();
+		
+
+	}
+
+}
