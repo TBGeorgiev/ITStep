750dd262fef2be1b490d92a50ab8bbe6ca301a4d
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-05-30 13:50:51 +0300

    Update 03.Graph.java

--- Exercises/Seeburger/D.T./Graph/03.Graph.java
+++ Exercises/Seeburger/D.T./Graph/03.Graph.java
@@ -2,32 +2,61 @@ package com.seeburger.graph;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
 
-	private HashMap<Integer, Node> nodeLookup = new HashMap<Integer, Node>();
+	private HashMap<String, Node> nodeLookup = new HashMap<String, Node>();
 	
-	public HashMap<Integer, Node> getNodes() {
+	public HashMap<String, Node> getNodes() {
 		return this.nodeLookup;
 	}
 
-	private Node getNode(int id) {
+	private Node getNode(String id) {
 		Node node = new Node(id);
 		nodeLookup.putIfAbsent(id, node);
 		return nodeLookup.get(id);
 	}
 
-	public void addEdge(int source, int destination) {
+	public void addEdge(String source, String destination) {
 		Node sNode = getNode(source);
 		Node dNode = getNode(destination);
+		dNode.addAdjacentToNode(sNode);
 		sNode.addAdjacentToNode(dNode);
 	}
+	
+	public void bredthFirstSearch(String source, String destination) {
+		LinkedHashSet<String> visited = new LinkedHashSet<String>();
+		
+		Queue<String> queue = new LinkedList<String>();
+		visited.add(source);
+		queue.add(source);
+		
+		while (queue.size() != 0) {
+			source = queue.poll();
+			System.out.println(source + " ");
+			if (source == destination) {
+				break;
+			}
+			
+			Iterator<Node> iterator = nodeLookup.get(source).getAdjacent().listIterator();
+			while (iterator.hasNext()) {
+				String nString = iterator.next().getId();
+				if (!visited.contains(nString)) {
+					visited.add(nString);
+					queue.add(nString);
+				}
+			}		
+		}		
+	}
 
-	public boolean hasPathDFS(int source, int destination) {
+	public boolean hasPathDFS(String source, String destination) {
 		Node sNode = getNode(source);
 		Node dNode = getNode(destination);
-		LinkedHashSet<Integer> visited = new LinkedHashSet<Integer>();
+		LinkedHashSet<String> visited = new LinkedHashSet<String>();
 		if (hasPathDFS(sNode, dNode, visited)) {
 			Object[] intArray = visited.toArray();
 			for (int i = 0; i < intArray.length; i++)
@@ -41,7 +70,7 @@ public class Graph {
 		}
 	}
 
-	private boolean hasPathDFS(Node sNode, Node dNode, HashSet<Integer> visited) {
+	private boolean hasPathDFS(Node sNode, Node dNode, HashSet<String> visited) {
 		if (visited.contains(sNode.getId())) {
 			return false;
 		}
