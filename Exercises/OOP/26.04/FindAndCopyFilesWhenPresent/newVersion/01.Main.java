f23a0a871b8752f071511a91254b943006195083
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-06-01 02:10:47 +0300

    Update 01.Main.java

--- Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/01.Main.java
+++ Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/01.Main.java
@@ -3,10 +3,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException, IOException {
+	public static void main(String[] args) {
 		ExecutorService executorService = Executors.newFixedThreadPool(5);
 		Finder finder = new Finder(executorService);
-		finder.transferFiles();		
+		try {
+			finder.transferFiles();
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 		executorService.shutdown();
 	}
 }
