76d802d677a59e8101b938756cdf746a55280189
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-06-15 01:22:06 +0300

    Update 02.Finder.java

--- Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/02.Finder.java
+++ Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/02.Finder.java
@@ -1,28 +1,48 @@
+package com.seeburger.fileTransfer;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.concurrent.ExecutorService;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Finder {
 	private ExecutorService executorService;
-	private boolean emptyFolder;
 	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	private final String enterDirectoryToSearch = "Enter a directory to search:";
-	private final String enterCopyDestination = "Enter the destination you want to copy the files to:";
+	private boolean emptyFolder;
+	private static final String DIRECTORY_TO_SEARCH = "Enter a directory to search:";
+	private static final String MOVE_DESTINATION = "Enter the destination you want to copy the files to:";
+	private Logger logger = Logger.getLogger("FileLog");
+	private FileHandler fHandler;
 
 	public Finder(ExecutorService executorService) {
 		this.executorService = executorService;
+		try {
+			fHandler = new FileHandler("FileLog.log");
+			logger.addHandler(fHandler);
+			SimpleFormatter simpleFormatter = new SimpleFormatter();
+			fHandler.setFormatter(simpleFormatter);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	// Starts main transfer method
 	public void transferFiles() throws IOException, InterruptedException {
-		System.out.println(enterDirectoryToSearch);
+		System.out.println(DIRECTORY_TO_SEARCH);
 		String directory = reader.readLine();
-		System.out.println(enterCopyDestination);
+		System.out.println(MOVE_DESTINATION);
 		String destination = reader.readLine();
 		lookForFiles(directory, destination);
 	}
 
+	// Performs checks and starts a file moving thread if files are present
 	public void lookForFiles(String location, String destination) throws InterruptedException, IOException {
 		File folder = new File(location);
 		if (!folder.exists()) {
@@ -37,21 +57,35 @@ public class Finder {
 				System.out.println("Location is empty. Waiting for files to arrive.");
 			}
 			emptyFolder = true;
-			Thread.currentThread().sleep(100);
+			Thread.currentThread();
+			Thread.sleep(100);
 			lookForFiles(location, destination);
 			return;
 		}
 		emptyFolder = false;
-		RunnableClass runnableClass = new RunnableClass(files, destination);
+		RunnableClass runnableClass = new RunnableClass(location, destination, logger);
 		executorService.execute(runnableClass);
+		continueOperations(runnableClass);
+	}
+
+	// Main thread waits for user input to start a new operation or end
+	private void continueOperations(RunnableClass runnableClass) throws IOException, InterruptedException {
+		System.out.println("Enter 'end' to terminate the operation or enter 'y' to look for other files.");
 
 		String toContinue = reader.readLine();
 		if (toContinue.equalsIgnoreCase("y")) {
-			System.out.println(enterDirectoryToSearch);
+			System.out.println(DIRECTORY_TO_SEARCH);
 			String loc = reader.readLine();
-			System.out.println(enterCopyDestination);
+			System.out.println(MOVE_DESTINATION);
 			String dest = reader.readLine();
 			lookForFiles(loc, dest);
+			return;
+		} else if (toContinue.equalsIgnoreCase("end")) {
+			System.out.println("Closing...");
+			runnableClass.setToStop(true);
+		} else {
+			System.out.println("Incorrect input");
+			continueOperations(runnableClass);
 			return;
 		}
 	}
