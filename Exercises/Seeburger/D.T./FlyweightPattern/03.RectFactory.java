13807a85d59d690781bef15a55ebf6472a3981b6
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-06-25 10:51:10 +0300

    Update 03.RectFactory.java

--- Exercises/Seeburger/D.T./FlyweightPattern/03.RectFactory.java
+++ Exercises/Seeburger/D.T./FlyweightPattern/03.RectFactory.java
@@ -3,32 +3,16 @@ package com.seeburger.flyweight;
 //This factory only creates a new rectangle if it 
 //uses a color not previously used
 
-//Intrinsic State: Color
-//Extrinsic State: X & Y Values
-
 import java.util.HashMap;
 import java.awt.Color;
-public class RectFactory {
-	
-	// The HashMap uses the color as the key for every 
-	// rectangle it will make up to 8 total
-	
+public class RectFactory {	
 	private static final HashMap<Color, MyRect> rectsByColor = new HashMap<Color, MyRect>();
 
 	public static MyRect getRect(Color color) {
-		MyRect rect = (MyRect)rectsByColor.get(color);
-		
-		// Checks if a rectangle with a specific
-		// color has been made. If not it makes a
-		// new one, otherwise it returns one made already
-		
+		MyRect rect = (MyRect)rectsByColor.get(color);	
 		if(rect == null) {
-			rect = new MyRect(color);
-			
-			// Add new rectangle to HashMap
-			
+			rect = new MyRect(color);			
 			rectsByColor.put(color, rect);
-      
 		}
 		return rect;
 	}
