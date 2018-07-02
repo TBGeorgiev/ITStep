6ce25d88f9b0b11b63c8709746fcc4c7f83b493a
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-07-02 16:22:52 +0300

    Update 04.ConsistencyChecker.java

--- Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/04.ConsistencyChecker.java
+++ Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/04.ConsistencyChecker.java
@@ -1,22 +1,20 @@
 package com.seeburger.fileTransferAutomation;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.security.MessageDigest;
 import java.util.ArrayList;
-import java.util.LinkedList;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class ConsistencyChecker implements Runnable
 {
-	// private File[] files;
 	private RunnableClass runnableClass;
 	private String destinationString;
-	private LinkedList<String> bytesList;
+	private LinkedHashMap<String, String> bytesList;
 
-	public ConsistencyChecker(String destinationString, RunnableClass runnableClass, LinkedList<String> fileByteStrings)
+	public ConsistencyChecker(String destinationString, RunnableClass runnableClass,
+			LinkedHashMap<String, String> fileByteStrings)
 	{
-		// this.files = files;
 		this.runnableClass = runnableClass;
 		this.destinationString = destinationString;
 		this.bytesList = fileByteStrings;
@@ -52,56 +50,33 @@ public class ConsistencyChecker implements Runnable
 		}
 	}
 
-	private byte[] createChecksum(String filename) throws Exception
-	{
-		InputStream fis = new FileInputStream(filename);
-
-		byte[] buffer = new byte[1024];
-		MessageDigest complete = MessageDigest.getInstance("MD5");
-		int numRead;
-
-		do
-		{
-			numRead = fis.read(buffer);
-			if (numRead > 0)
-			{
-				complete.update(buffer, 0, numRead);
-			}
-		} while (numRead != -1);
-
-		fis.close();
-		return complete.digest();
-	}
-
-	private void checkFiles(String destinationString, LinkedList<String> bytesList2) throws Exception
+	private void checkFiles(String destinationString, LinkedHashMap<String, String> bytesList2) throws Exception
 	{
 		ArrayList<String> sourceByteStrings = new ArrayList<String>();
 		File folder = new File(destinationString);
 		File[] files = folder.listFiles();
-		System.out.println(files.length);
 		for (File file : files)
 		{
 			if (!file.isDirectory())
 			{
-				String result = "";
-				byte[] b1 = createChecksum(file.getAbsolutePath());
-				for (int j = 0; j < b1.length; j++)
-				{
-					result += Integer.toString((b1[j] & 0xff) + 0x100, 16).substring(1);
-				}
+				String result = ChecksumUtilities.getMD5(file);
 				sourceByteStrings.add(result);
 			}
 		}
 
-		for (int i = 0; i < bytesList2.size(); i++)
+		int index = 0;
+
+		for (Map.Entry<String, String> map : bytesList2.entrySet())
 		{
-			if (!bytesList2.get(i).equals(sourceByteStrings.get(i)))
-			{
-				System.out.println("Corrupted file");
-			} else
+//			System.out.println(map.getValue() + " : " + sourceByteStrings.get(index));
+			if (map.getValue().equals(sourceByteStrings.get(index)))
 			{
 				System.out.println("Good");
+			} else
+			{
+				System.out.println("Bad file " + map.getKey());
 			}
+			 index++;
 		}
 	}
 }
