28e3f19e23e9f9ec21e5ab41eef34592d3ea8b07
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-07-02 16:23:18 +0300

    Create 05.ChecksumUtilities.java

---
+++ Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/05.ChecksumUtilities.java
@@ -0,0 +1,47 @@
+package com.seeburger.fileTransferAutomation;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+public final class ChecksumUtilities
+{
+	public static byte[] createChecksum(String filename) throws Exception
+	{
+		InputStream fis = new FileInputStream(filename);
+
+		byte[] buffer = new byte[1024];
+		MessageDigest complete = MessageDigest.getInstance("MD5");
+		int numRead;
+
+		do
+		{
+			numRead = fis.read(buffer);
+			if (numRead > 0)
+			{
+				complete.update(buffer, 0, numRead);
+			}
+		} while (numRead != -1);
+
+		fis.close();
+		return complete.digest();
+	}
+	
+	public static String getMD5(File fileLocation) {
+		String result = "";
+		byte[] b1;
+		try
+		{
+			b1 = createChecksum(fileLocation.getAbsolutePath());
+			for (int j = 0; j < b1.length; j++)
+			{
+				result += Integer.toString((b1[j] & 0xff) + 0x100, 16).substring(1);
+			}
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
