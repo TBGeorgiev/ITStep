9f37396fe6fd494114634f8d6a75d1e9b3ba701c
Tsvetoslav Georgiev <32303656+TBGeorgiev@users.noreply.github.com>
2018-07-03 14:12:58 +0300

    Update 01.Main.java

--- Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/01.Main.java
+++ Exercises/OOP/26.04/FindAndCopyFilesWhenPresent/newVersion/01.Main.java
@@ -1,18 +1,26 @@
-package com.seeburger.fileTransfer;
+package com.seeburger.files;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Main {
-	public static void main(String[] args) {
+public class Main
+{
+	public static void main(String[] args)
+	{
 		ExecutorService executorService = Executors.newFixedThreadPool(100);
-		Finder finder = new Finder(executorService);
-		try {
+		
+		//first boolean is for file integrity tests
+		//second boolean is for location and destination tests
+		Finder finder = new Finder(executorService, true, true);
+		try
+		{
 			finder.transferFiles();
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			e.printStackTrace();
-		} catch (InterruptedException e) {
+		} catch (InterruptedException e)
+		{
 			e.printStackTrace();
 		}
 		executorService.shutdown();
